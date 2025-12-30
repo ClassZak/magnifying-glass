e61a3acfc2100fb1cd872fc9a873dfb73deadb7d
ClassZak <pahargolub2@gmail.com>
2025-12-30 20:18:53 +0300

    Create runnable win32 app

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <windows.h>
+#include <iostream>
+
+int APIENTRY wWinMain(	_In_ HINSTANCE hInstance,
+						_In_opt_ HINSTANCE hPrevInstance,
+						_In_ LPWSTR lpCmdLine,
+						_In_ int nCmdShow)
+{
+	int a{456};
+
+	a = 220;
+}
+
